0c333fe3f789130611539448e07589a3a416c564
Vie <yangchenzhong@gmail.com>
2018-03-12 07:31:47 +0000

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -1,3 +1,6 @@
+#!/bin/env python
+
+# python lucky.py /path/to/snapshot.txt
 
 import os,binascii,sys
 from time import gmtime, strftime
@@ -9,12 +12,12 @@ snapshot = sys.argv[1]
 with open(snapshot) as afile:
     astr = afile.read()
     print("Open file {} ok....".format(snapshot))
-    with open("lucky{}.log".format(strftime("%Y%m%d", gmtime())), 'a') as l:
-        l.write("lucky..\n")
-        l.close()
     while True:
         pkey = binascii.b2a_hex(os.urandom(32))
         raw_address = utils.privtoaddr("{}".format(pkey))
         account_address = utils.checksum_encode(raw_address)
         if account_address in astr:
             print(pkey, account_address)
+            with open("lucky{}.log".format(strftime("%Y%m%d", gmtime())), 'a') as l:
+                l.write("{} {}\n".format(pkey, account_address))
+                l.close()
